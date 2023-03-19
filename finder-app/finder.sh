499e2fd21ec8b447c838dc052fb4f64635070c71
Nicole Milligan <nicole.milligan@colorado.edu>
2023-03-19 17:53:06 -0600

    submitting project

---
+++ finder-app/finder.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+filesdir=$1
+searchstr=$2
+
+if [ -z $filesdir ] || [ ! -d $filesdir ] || [ -z $searchstr ]
+then
+	echo 'Must supply a valid file directory and search string'
+	exit 1
+fi
+
+file_count=$(ls -ApFR $filesdir | grep -v @$ | wc -l)
+search_count=$(grep -R $searchstr $filesdir | wc -l)
+echo The number of files are $file_count and the number of matching lines are $search_count;
---
+++ finder-app/writer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+writefile=$1
+writestr=$2
+
+if [ -z $writefile ] || [ -z $writestr ];
+then
+	echo 'Must supply a valid file directory'
+	exit 1
+fi
+
+mkdir -p $(dirname $writefile) && touch $writefile && echo $writestr > $writefile echo 'Write failed'; exit 1
