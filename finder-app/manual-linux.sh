3ae7a79f4ad84c3a9c200447a0b12f1688e964c5
nik-ubuntu <nik-ubuntu@My-ubuntu.myguest.virtualbox.org>
2023-04-15 14:55:18 -0600

    generic dep path

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -83,12 +83,11 @@ ${CROSS_COMPILE}readelf -a busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
-SYSROOT=/home/nik-ubuntu/Documents/ecea_5305/arm-cross-compiler/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc
 
-cp ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
-cp ${SYSROOT}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
-cp ${SYSROOT}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
-cp ${SYSROOT}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
+sudo find / -name ld-linux-aarch64.so.1 -print -quit| xargs cp -t ${OUTDIR}/rootfs/lib
+sudo find / -name libm.so.6 -print -quit| xargs cp -t ${OUTDIR}/rootfs/lib64
+sudo find / -name libresolv.so.2 -print -quit| xargs cp -t ${OUTDIR}/rootfs/lib64
+sudo find / -name libc.so.6 -print -quit| xargs cp -t ${OUTDIR}/rootfs/lib64
 
 # TODO: Make device nodes
 cd "${OUTDIR}/rootfs"
