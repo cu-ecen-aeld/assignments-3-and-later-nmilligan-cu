8cf557cd25284629b64b064b85cadcd7d6e45246
nik-ubuntu <nik-ubuntu@My-ubuntu.myguest.virtualbox.org>
2023-04-22 15:36:10 -0600

    finder updates

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,7 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+username=$(cat /etc/conf/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -32,7 +32,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat /etc/conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -51,6 +51,9 @@ fi
 echo "Removing the old writer utility and compiling as a native application"
 #make clean
 #make
+
+# navigate to script directory 
+cd `dirname $0`
 
 for i in $( seq 1 $NUMFILES)
 do
